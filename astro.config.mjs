f5ff9edda645f9fa722096e45c3bd99000c26603
SOLOMEPAREDES <98500646+AntaresGT@users.noreply.github.com>
2025-06-29 14:27:51 -0600

    configuración de idiomas

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,8 +6,32 @@ import starlight from '@astrojs/starlight';
 export default defineConfig({
 	integrations: [
 		starlight({
-			title: 'My Docs',
-			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
+			title: {
+				es: 'Lenguaje Quetzal',
+				en: 'Quetzal Language',
+				id: 'Bahasa Quetzal',
+				ja: 'ケツァル語',
+			},
+			defaultLocale: 'es',
+			locales: {
+				es: {
+					label: 'Español',
+					lang: 'es',
+				},
+				en: {
+					label: 'English',
+					lang: 'en',
+				},
+				id: {
+					label: 'Bahasa Indonesia',
+					lang: 'id',
+				},
+				ja: {
+					label: '日本語',
+					lang: 'ja',
+				}
+			},
+			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/AntaresGT/lenguaje-quetzal' }],
 			sidebar: [
 				{
 					label: 'Guides',
