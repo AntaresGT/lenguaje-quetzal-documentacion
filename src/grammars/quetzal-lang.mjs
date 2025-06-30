0642d1d1ccc6c0587bddeb82d975f9bf3b946b9f
SOLOMEPAREDES <98500646+AntaresGT@users.noreply.github.com>
2025-06-29 22:27:56 -0600

    arreglando archivo grammar

--- src/grammars/quetzal-lang.mjs
+++ src/grammars/quetzal-lang.mjs
@@ -6,16 +6,15 @@ import { fileURLToPath } from 'node:url';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname  = path.dirname(__filename);
 
+const quetzalGrammar = JSON.parse(
+  fs.readFileSync(path.join(__dirname, 'quetzal.tmLanguage.json'), 'utf8')
+)
+
 export const quetzalLang = {
   /** scopeName debe coincidir con el de tu .tmLanguage */
   scopeName: 'source.quetzal',
   /** alias para las vallas ```qz */
-  aliases: ['qz'],
+  aliases: ['qz', 'quetzal'],
   /** el JSON de la gramática */
-  grammar: JSON.parse(
-    fs.readFileSync(
-      path.join(__dirname, 'quetzal.tmLanguage.json'),
-      'utf8'
-    )
-  ),
+  ...quetzalGrammar,
 };
